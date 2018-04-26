701aa68f4f8b146a2ce76bdce169777367bb0428
Keiho Sakapon <git@saka-pon.net>
2018-04-26 16:51:50 +0900

    Add test

--- Commandable/TestConsole/Program.cs
+++ Commandable/TestConsole/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Commandable;
 
 namespace TestConsole
 {
@@ -10,6 +11,29 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
+            Commands.UseEntryType();
+        }
+
+        // app -?
+        // app -help
+        [Command("Shows the help.")]
+        static void Help() => Commands.HelpFromEntryType();
+
+        // app file1.txt file2.txt
+        [DefaultCommand("Echoes the arguments.")]
+        public static void EchoArgs(string[] args)
+        {
+            foreach (var arg in args)
+                Console.WriteLine(arg);
+        }
+
+        // app -add 12 34
+        [Command("Adds 2 integers.")]
+        public static int Add(int x, int y)
+        {
+            var result = x + y;
+            Console.WriteLine(result);
+            return result;
         }
     }
 }

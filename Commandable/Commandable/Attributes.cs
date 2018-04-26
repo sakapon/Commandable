34e0c8626661d215e390a1fa0bdd074280efef91
Keiho Sakapon <git@saka-pon.net>
2018-04-26 15:14:55 +0900

    Add classes for attributes

---
+++ Commandable/Commandable/Attributes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Commandable
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public abstract class BaseCommandAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets the description of the command.
+        /// </summary>
+        public string Description { get; }
+
+        protected BaseCommandAttribute(string description)
+        {
+            Description = description;
+        }
+    }
+
+    /// <summary>
+    /// Marks the method as the default command action.
+    /// </summary>
+    public sealed class DefaultCommandAttribute : BaseCommandAttribute
+    {
+        public DefaultCommandAttribute(string description) : base(description) { }
+    }
+
+    /// <summary>
+    /// Marks the method as the command action.
+    /// </summary>
+    public sealed class CommandAttribute : BaseCommandAttribute
+    {
+        /// <summary>
+        /// Gets or sets the command name. This property is optional.
+        /// </summary>
+        public string CommandName { get; set; }
+
+        public CommandAttribute(string description) : base(description) { }
+    }
+}

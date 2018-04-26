50c80812332200f659cc0cebda963af6ac2ec637
Keiho Sakapon <git@saka-pon.net>
2018-04-26 11:32:16 +0900

    Update project settings

--- Commandable/Commandable/Properties/AssemblyInfo.cs
+++ Commandable/Commandable/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,13 +7,17 @@ using System.Runtime.InteropServices;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("Commandable")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The library to equip a console application with the command line interface.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Commandable")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Keiho Sakapon")]
+[assembly: AssemblyProduct("KLibrary")]
+[assembly: AssemblyCopyright("© 2018 Keiho Sakapon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyMetadata("ProjectUrl", "https://github.com/sakapon/Commandable")]
+[assembly: AssemblyMetadata("LicenseUrl", "https://github.com/sakapon/Commandable/blob/master/LICENSE")]
+[assembly: AssemblyMetadata("Tags", "Command Line Console")]
+[assembly: AssemblyMetadata("ReleaseNotes", "The first release.")]
 
 // ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
 // 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
@@ -33,4 +38,7 @@ using System.Runtime.InteropServices;
 // 以下のように '*' を使用します:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+
+[assembly: CLSCompliant(true)]
+[assembly: InternalsVisibleTo("UnitTest")]

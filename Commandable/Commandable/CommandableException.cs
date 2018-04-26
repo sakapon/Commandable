a39eb958d299d008da4a7f7ebd6920a7fc3efa64
Keiho Sakapon <git@saka-pon.net>
2018-04-26 16:21:11 +0900

    Add classes

---
+++ Commandable/Commandable/CommandableException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Commandable
+{
+    [Serializable]
+    public class CommandableException : Exception
+    {
+        public CommandableException() { }
+        public CommandableException(string message) : base(message) { }
+        public CommandableException(string message, Exception inner) : base(message, inner) { }
+        protected CommandableException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ Commandable/Commandable/Commands.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Commandable
+{
+    public static class Commands
+    {
+        public static void UseEntryType()
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch (CommandableException ex)
+            {
+                Console.WriteLine($"Implementation Error: {ex.Message}");
+                Environment.ExitCode = 1;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected Error: {ex}");
+                Environment.ExitCode = 2;
+            }
+        }
+
+        public static void HelpFromEntryType()
+        {
+            Console.WriteLine("TODO: Show Help.");
+        }
+    }
+}

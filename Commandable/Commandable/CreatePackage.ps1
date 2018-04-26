0ba27e7bd1687d424c0b4bf3175f7bc85bb4223f
Keiho Sakapon <git@saka-pon.net>
2018-04-26 13:55:04 +0900

    Add tool to create package

---
+++ Commandable/Commandable/CreatePackage.ps1
@@ -0,0 +1,13 @@
+﻿# $msbuild = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+$msbuild = "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
+
+.\KTools.VersionIncrement.ps1
+
+& $msbuild /p:Configuration=Release /t:Clean
+& $msbuild /p:Configuration=Release /t:Rebuild
+
+.\NuGetPackup.exe
+
+ni nupkg -type directory -Force
+move *.nupkg nupkg -Force
+explorer nupkg
